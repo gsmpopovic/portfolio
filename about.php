6f98e434026ff14fb3431fb0c02b65ab409f640a
George Popovic <georgesmpopovic@gmail.com>
2020-07-14 21:31:38 -0400

    First commit.

---
+++ about.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+    <link href='style.css' type='text/css' rel='stylesheet'>
+</head>
+<body>
+    <?php include('nav.php')?>
+
+    <h1>About Me</h1>
+     
+    <p>I'm an aspiring web developer from NYC, and my work is done through the WAMP stack 
+        (PHP is my backend language, and I'd love to learn Python).</p>
+
+    <h2>My background<h2>
+
+    I have a BA in Classical Languages and Philosophy from Fordham University (2019), and have been interested in web development and programming in general since June of 2018. 
+    
+    <h2>My interests<h/2>
+
+    <p> As a developer with a background in Classical Languages and Philosophy, 
+        I'm very interested in their intersection, the "digital humanities", 
+        and how web technologies and procedures can be used to display, 
+        interact with, and shed new light on the study of classical and philosophical texts.</p> 
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,16 @@
+<nav>
+    <ul>
+        <li>
+            <a href='index.php'>Home</a> 
+        </li>
+        <li>
+            <a href='about.php'>About</a> 
+        </li>
+        <li>
+            <a href='work.php'>Work</a> 
+        </li>
+        <li>
+            <a href='contact.php'>Contact</a> 
+        </li>
+    </ul> 
+</nav> 
