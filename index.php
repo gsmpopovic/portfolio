e068a40ee2233fd39d719ea9973f5e017c4b868d
George Popovic <georgesmpopovic@gmail.com>
2020-07-20 04:59:00 -0400

    Added more styling.

--- index.php
+++ index.php
@@ -4,16 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="./style.css" >
+    <link rel="stylesheet" type="text/css" href="./assets/style.css" >
+    <link href="https://fonts.googleapis.com/css2?family=Telex&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class='head-container'> 
+    <div class='split-head'></div>
     </div>
     <main>
         <div class='home-container'> 
             <div class='split-left'>
-                <h1 class='hello'>HE</h1>
-                <h1 class='hello'>LLO.<h1>
+                <h1 id='hello'>Hello.<h1>
             </div>
             <div class='split-right'>
             <?php include_once('nav.php')?>
